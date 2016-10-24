49ac3bdcd1ca4f3c29fff072f258665167a309c2
honggyu kim <kimhongka123@gmail.com>
2016-10-24 11:30:32 +1000

    initial

---
+++ MainActivity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace PopupWindowDemo
+{
+    [Activity(Label = "PopupWindowDemo", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        private Button popupWindowButton;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.Main);
+            popupWindowButton = FindViewById<Button>(Resource.Id.PopUpWindow_Button);
+            popupWindowButton.Click += OnPopupWinowButtonClicked;
+         
+        }
+
+        private void OnPopupWinowButtonClicked(object sender, EventArgs e)
+        {
+            popupWindowButton.Enabled = false;
+
+            LayoutInflater inflater = (LayoutInflater)this.GetSystemService(LayoutInflaterService);
+            View popup = inflater.Inflate(Resource.Layout.PopupWindowContent,null);
+            PopupWindow window = new PopupWindow(popup, ViewGroup.LayoutParams.WrapContent, ViewGroup.LayoutParams.WrapContent);
+            window.ShowAtLocation(popup, GravityFlags.Center, 0, 0);
+            window.ShowAsDropDown(popupWindowButton);
+
+            Button closeButton = popup.FindViewById<Button>(Resource.Id.CloseButton);
+            closeButton.Click += delegate {
+                window.Dismiss();
+            };
+            popupWindowButton.Enabled = true;
+        }
+    }
+}
+
